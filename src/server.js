04f4de5b4cc564cf9514d91d67e0de97903b2852
donkey_kong <donghui8266@gmail.com>
2022-11-16 12:51:53 +0900

    websocket으로 간단한 채팅 구현

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,8 +9,36 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected from Browser ❌");
+  });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString("utf-8"));
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
+  socket.send("Hello!!!");
+});
+
+server.listen(3000, handleListen);
